94a17148808e921efdc6b90f60ad3c61110dad17
Atif Badini <badiniatif@gmail.com>
2023-11-03 18:39:14 +0500

    branch-test

--- src/utlis/services/product_category_services.js
+++ src/utlis/services/product_category_services.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+
+import { BASE_URL,API_VERSION,END_POINT,CATEGORY_ENDPOINT,CATEGORY_ITEMS_LIST_ENDPOINT } from "../apiUrls";
+
+export async function AgentList() {
+    let finalUrl = BASE_URL + API_VERSION()
+    const resp = await axios.get(finalUrl)
+    return resp.data
+}
