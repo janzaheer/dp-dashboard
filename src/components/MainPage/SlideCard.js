25df6ce572b49331d27cb1e1c9b3a6ce33f92651
Atif Badini <badiniatif@gmail.com>
2024-05-29 17:41:54 +0500

    build test

--- src/components/MainPage/SlideCard.js
+++ src/components/MainPage/SlideCard.js
@@ -14,7 +14,6 @@ const SlideCard = () => {
       id: 1,
       title: "50% Off For Your First Shopping",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      // cover: "https://royalpets.pk/wp-content/uploads/2023/10/dimond-family-2.jpg",
       cover: `${d2}`,
       price: 20000
     },
@@ -22,7 +21,6 @@ const SlideCard = () => {
       id: 2,
       title: "50% Off For Your First Shopping",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      // cover: "https://royalpets.pk/wp-content/uploads/2023/10/dimond-natural-grai-free.jpg",
       cover: `${d3}`,
       price: 2500
     },
